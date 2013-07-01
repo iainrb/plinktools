8fe22040ac95104f277f4a1f4c2046768ded6e73
Iain Bancarz <ib5@sanger.ac.uk>
2013-07-01 11:24:52 +0100

    Change --dir argument to --prefix, use as glob prefix instead of directory

--- merge_bed_multiple.py
+++ merge_bed_multiple.py
@@ -18,20 +18,21 @@
 
 # Author: Iain Bancarz, ib5@sanger.ac.uk
 
-"""Script to merge multiple Plink .bed files"""
+"""Script to merge two or more Plink binary datasets"""
 
 import argparse, json, sys
 from plink import PlinkMerger
 
 def main():
     """Method to run as script from command line"""
-    description = "Merge multiple Plink binary datasets"
-    epilog = "Default behaviour is to merge all plink binary datasets in the current working directory. Can change directory using --dir or specify a list of datasets using --stems. Specifying both --dir and --stems is not permitted."
+    description = "Merge two or more Plink binary datasets. Inputs must have either disjoint samples and congruent SNPs, or congruent SNPs and disjoint samples."
+    epilog = "Default behaviour is to merge all plink binary datasets in the current working directory. Can change directory and file prefix using --prefix or specify a list of datasets using --stems. Specifying both --prefix and --stems is not permitted."
     parser = argparse.ArgumentParser(description=description, epilog=epilog)
     parser.add_argument('--stems', required=False, metavar="PATH", 
                         help="Path to .json file listing Plink stems (paths without .bed, .bim, .fam extension). Optional, see below.")
-    parser.add_argument('--dir', required=False, metavar="PATH", default=None,
-                        help="Path to directory containing Plink binary datasets to merge. Optional, see below.")
+    parser.add_argument('--prefix', required=False, metavar="PATH", 
+                        default=None,
+                        help="Prefix for Plink binary datasets. Merge will glob all datasets of the form [prefix]*.bed.  Optional, see below.")
     parser.add_argument('--out', required=True, metavar="PATH", 
                         help="Plink stem (path without .bed, .bim, .fam extension) for merged output. Required.")
     parser.add_argument('--bim', metavar="PATH", 
@@ -39,14 +40,14 @@ def main():
     args = vars(parser.parse_args())
     pm = PlinkMerger()
     if args['stems']!=None:
-        if args['dir']!=None:
-            raise ValueError("Can specify at most one of --stems, --dir")
+        if args['prefix']!=None:
+            raise ValueError("Can specify at most one of --stems, --prefix")
         else:
             stemList = json.loads(open(args['stems']).read())
     else:
-        if args['dir']==None: inputDir = "."
-        else: inputDir = args['dir']
-        stemList = pm.findBedStems(inputDir)
+        if args['prefix']==None: inputPrefix = "."
+        else: inputPrefix = args['prefix']
+        stemList = pm.findBedStems(inputPrefix)
     pm.merge(stemList, args['out'], args['bim'])
 
 if __name__ == "__main__":
